aa35b5e0f354df5b4b159979ae97b66dcb0beb3c
DW2733 <dw2733@tc.columbia.edu>
2017-03-27 19:58:26 -0400

    new project

--- app/src/main/java/com/example/dadriaunna01/readfromfirebase/MainActivity.java
+++ app/src/main/java/com/example/dadriaunna01/readfromfirebase/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.dadriaunna01.readfromfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView displayText;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference personRef = database.getReference("person");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText = (TextView) findViewById(R.id.display_text);
+        personRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Person p = dataSnapshot.getValue(Person.class);
+                displayText.setText(p.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Error loading Firebase", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/dadriaunna01/readfromfirebase/Person.java
@@ -0,0 +1,40 @@
+package com.example.dadriaunna01.readfromfirebase;
+
+/**
+ * Created by cmltdstudent on 3/27/17.
+ */
+
+
+public class Person {
+    private String name;
+    private int age;
+    private boolean alive;
+
+    public Person() {
+    } // Empty constructor is needed to read from Firebase
+
+    public Person(String name, int age, boolean alive) {
+        this.name = name;
+        this.age = age;
+        this.alive = alive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + " " + age + " " + alive;
+    }
+}
+
+
